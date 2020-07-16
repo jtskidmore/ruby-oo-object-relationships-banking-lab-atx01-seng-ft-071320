d0b1b566c3291d999c59180b2652c7fbdc9bc261
JT Skidmore <johntoddskidmore@gmail.com>
2020-07-16 18:34:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,9 +2,10 @@ require 'pry'
 
 class Transfer
 
-  def initialize (sender, receiver)
+  def initialize (sender, receiver, amount)
     @sender = sender
     @receiver = receiver
+    @amount = amount
     @status = "pending"
   end
 
